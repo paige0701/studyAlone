b31286cf35390bacf012b4be1a456bb23c13347c
Paige <cde0701@gmail.com>
2018-12-11 18:51:51 +0900

    Add if statement example

--- python/control-flow-tools/if-statement.py
+++ python/control-flow-tools/if-statement.py
@@ -1,1 +1,22 @@
+# elif is shorted version of else if
+# white space is extremely important in python
+# no brackets when using if statement
+
+
+def if_test():
+    x = int(input("Please enter an integer: "))
+    if x < 10:
+        print("Less than 10")
+    elif x < 0:
+        print("Less than 0")
+    else:
+        print("Bigger than 10")
+
+
+def main():
+    if_test()
+
+
+if __name__ == '__main__':
+    main()
 
