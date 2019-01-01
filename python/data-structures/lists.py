8ad8661796d24fb4146029b50edd6ce73ddd5d01
Paige <cde0701@gmail.com>
2019-01-01 15:30:39 +0900

    study tuples and sets

--- python/data-structures/lists.py
+++ python/data-structures/lists.py
@@ -156,6 +156,42 @@ def listmethods():
 
     print(list(zip(*matrix)))
 
+    # ------------------------------------
+    # Tuples
+    t = 123, 455, 677
+    print(t[0])  # 123
+    print(t)  # (123, 456, 677)
+
+    #  t[0] = 1  # error .. can't assign value with tuple
+
+    empty = ()
+    print(len(empty))  # 0
+
+    singleton = 'hello',
+    print(singleton)
+    print(len(singleton))  # 1 if singleton - 'hello' with no comma at the end, len(singleton) is 5
+
+    # ------------------------------------
+    # Sets : Curly braces can be used or set() function is used to make sets
+    fruits = {'apple', 'mango', 'apple', 'mango'}  # duplicates are removed
+    print(fruits)  # {'apple', 'mango'}
+
+    print('mango' in fruits)  # True
+
+    a = set('Banana')
+    b = set('Bananas in pajamas')
+    print(a)  # {'n', 'a', 'B'}  - unique letters in a
+    print(b)  # {'m', 'n', 'j', 'B', 'i', ' ', 'a', 'p', 's'} - unique letters in b
+    print(a - b)  # set() - letters in a but not in b
+    print(b - a)  # {' ', 'j', 'p', 'i', 's', 'm'} - letters in b but not in a
+    print(a | b)  # {'m', 'B', ' ', 'i', 'p', 'a', 'n', 's', 'j'} - letters in a and b
+    print(a & b)  # {'B', 'a', 'n'} - letters in a and b
+    print(a ^ b)  # {'j', 'p', 'i', ' ', 's', 'm'} - letters in a or b but not in both
+
+    a = {x for x in 'abracadabra' if x not in 'abc'}
+    print(a)  # {'d', 'r'}
+
+
 
 def main():
     listmethods()
